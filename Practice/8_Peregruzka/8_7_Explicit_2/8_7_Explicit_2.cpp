2017c5435656397c7cdcd57cc7133c450d128d56
Vkapeschin <kapeshin-vladimir@rambler.ru>
2024-01-20 22:51:47 +0300

    Explicit_2 from book

--- Practice/8_Peregruzka/8_7_Explicit_2/8_7_Explicit_2.cpp
+++ Practice/8_Peregruzka/8_7_Explicit_2/8_7_Explicit_2.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
-class New
+class Distance
 {
+    private:
+        const float MTF;
+        int feet;
+        float inches;
+    public:
+        Distance() : feet(0), inches(0), MTF(3.280833F) {}
+        explicit Distance(float meters) : MTF(3.280833F)
+        {
+            int fullfeet = meters * MTF;
+            feet = int(fullfeet);
+            inches = fullfeet - feet;
+        }
+        void show_dist()
+        {cout << "Distanse in feet: " << feet << "'" << inches << endl;}
+};
+
+int main()
+{
+    void fancy(Distance);
+    char ch;
+    while(ch != 'n')
+        { 
+        int metrs;
+        cout << "Input meters: "; cin >> metrs;
+        Distance d1(metrs);
+
+        cout << "Press 'n' for break!";
+        }
     
+    return 0;
 }
