da2c3fe39249ee9fd6bdcb8bb00553337a2e9e29
Vkapeschin <kapeshin-vladimir@rambler.ru>
2024-01-22 23:27:22 +0300

    Complete Mutable

--- Practice/8_Peregruzka/8_8_Mutable/8_8_Mutable.cpp
+++ Practice/8_Peregruzka/8_8_Mutable/8_8_Mutable.cpp
@@ -2,3 +2,23 @@
 using namespace std;
 
 class New
+{
+    private:
+        int number_1;
+        mutable int number_2;
+    public:
+        New(): number_1(0), number_2(1) {}
+    void show_number() const
+    { cout << number_1 << " " << number_2 << endl; }
+
+    void operator++ () const
+    { number_2++; }
+};
+
+int main()
+{
+    const New n1;
+    n1.show_number();
+    ++n1;
+    n1.show_number();
+}
